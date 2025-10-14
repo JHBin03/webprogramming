c9a2efb76acc62eea5398426085ce756f83d35ea
JHBin03 <huskyhbin@gmail.com>
2025-10-14 23:33:53 +0900

    Add files via upload

---
+++ 1001/HW06/js/6.1.js
@@ -0,0 +1,9 @@
+// 배경색을 노란색으로 바꾸는 함수
+function changeBackgroundYellow(element) {
+    element.style.background = 'yellow';
+}
+
+// 배경색을 흰색으로 바꾸는 함수
+function changeBackgroundWhite(element) {
+    element.style.background = 'white';
+}
